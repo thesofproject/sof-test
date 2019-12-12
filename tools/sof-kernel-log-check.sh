b3b7d8df9c84df10eb867c5c84c8b6c46a62dac2
Pan Xiuli <xiuli.pan@linux.intel.com>
2019-12-12 14:45:46 +0800

    tools: Init for sof-kernel-log-check.sh
    Check from if sof related error messages showed in /var/log/kern.log
    Accept number argument for start line to do the check.
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

---
+++ tools/sof-kernel-log-check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+begin_line=${1:-1}
+declare err_str ignore_str project_key
+err_str="error|failed|timed out|panic|oops"
+ignore_str="no reply expected,|error: debugfs write failed to idle -16"
+project_key="sof-audio"
+
+[[ ! "$err_str" ]] && echo "Missing error keyword list" && exit 0
+# dmesg KB size buffer size
+#dmesg_config_define=$(awk -F '=' '/CONFIG_LOG_BUF_SHIFT/ {print $2;}' /boot/config-$(uname -r))
+#dmesg_buffer_size=$( echo $(( (1<<$dmesg_config_define) / 1024 )) )
+# kernel file log buffer size
+#kernel_buffer_size=$(du -k /var/log/kern.log |awk '{print $1;}')
+# now decide using which to catch the kernel log
+#[[ $kernel_buffer_size -lt $dmesg ]] && cmd="dmesg" || cmd="sed -n '$begin_line,\$p' /var/log/kern.log"
+
+# confirm begin_line is number, if it is not the number, direct using dmesg
+[[ "${begin_line//[0-9]/}" ]] && begin_line=0
+[[ "$begin_line" -eq 0 ]] && cmd="dmesg" || cmd="sed -n '$begin_line,\$p' /var/log/kern.log"
+
+#echo "run $0 with parameter '$*' for check kernel message error"
+
+if [ "$ignore_str" ]; then
+    err=$(eval $cmd|grep 'Call Trace')$(eval $cmd | grep $project_key | grep -E "$err_str"|grep -vE "$ignore_str")
+else
+    err=$(eval $cmd|grep 'Call Trace')$(eval $cmd | grep $project_key | grep -E "$err_str")
+fi
+
+if [ "$err" ]; then
+    echo "Caught error: $err"
+    echo "kernel log has error(s)"
+    builtin exit 1
+fi
+
+builtin exit 0
