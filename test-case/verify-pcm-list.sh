1e09d40ac91449ac0bc4f23fda2dbac428642e07
Wu, BinX <binx.wu@intel.com>
2020-01-02 16:09:06 +0800

    Bug fix: dectect error TPLG file in verify-pcm-list
    the root reason: when load command the WD (workdir) is $PWD,
    from ssh the WD is $HOME
    
    When have the same name TPLG file on the WD which is relative path
    it can be detect, and verify the test case for this TPLG
    
    now modify to load from TPLG_ROOT for the same name TPLG is 1st option
    On the other hand convert the relative path TPLG to absolute path
    
    So when review case log can finger out which TPLG verify by test-case
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>

--- test-case/verify-pcm-list.sh
+++ test-case/verify-pcm-list.sh
@@ -77,10 +77,10 @@ do
     # expect left ',' 1st filed
     tplg=${tplg#*,}
     [ "$tplg_file" == "$tplg" ] && tplg=""
-    if [ -f "$tplg_file" ]; then
-        tplg_file="$f"
-    elif [ -f "$TPLG_ROOT/$tplg_file" ]; then
-        tplg_file="$TPLG_ROOT/$tplg_file"
+    if [ -f "$TPLG_ROOT/$(basename $tplg_file)" ]; then
+        tplg_file="$TPLG_ROOT/$(basename $tplg_file)"   # catch from TPLG_ROOT
+    elif [ -f "$tplg_file" ];then
+        tplg_file=$(realpath $tplg_file)    # relative path -> absolute path
     else
         dloge "Couldn't find target TPLG file $tplg_file needed to run ${BASH_SOURCE[0]}" && exit 1
     fi
