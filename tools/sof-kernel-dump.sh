9410925d2d34a5029875cf6da0a2276a81b6de83
Wu, BinX <binx.wu@intel.com>
2020-01-16 16:28:32 +0800

    tools: speed up sof-kernel-dump
    1. use tail instead of sed command
    2. add last kernel parameter, for example:
       'sof-kernel-dump.sh' dump the last one kernel
       'sof-kernel-dump.sh 1' dump the second last
    
    compare script spend time with 'time' command:
    file size: 1.8G
    |      | old      | new      |
    | ---- | -------- | -------- |
    | real | 0m2.523s | 0m1.277s |
    | user | 0m1.689s | 0m0.672s |
    | sys  | 0m0.651s | 0m0.415s |
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>

--- tools/sof-kernel-dump.sh
+++ tools/sof-kernel-dump.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-log_file="/var/log/kern.log"
-bootup_keyword='\[    0.000000\] Linux version '"$(uname -r)"
-# tr '\0' ' ' to fix warning message: "command substitution: ignored null byte in input"
-base_info=$(grep -na "$bootup_keyword" $log_file|tr '\0' ' ')
-# rollback to kern.log.1, OS will write the log into the different file
-# so here when current log file: kern.log miss the system boot information
-# rollback catch from kern.log.1, normally just 1 file is enough to cache it
-[[ ! "$base_info" ]] && log_file="/var/log/kern.log.1"
-boot_line=$(grep -na "$bootup_keyword" $log_file |tr '\0' ' '|sed -n '$p'|awk -F ':' '{print $1;}')
-[[ ! "$boot_line"  ]] && boot_line=0
-#sed -n "$boot_line,\$p" $log_file |awk '{ for(i=1; i<=4; i++){ $i="" }; print $0 }'|sed 's/^    //g'
-sed -n "$boot_line,\$p" $log_file |cut -f5- -d ' '
-[[ "$base_info" ]] && exit
-# this logic means not find bootup information in the kern.log file, but find at kern.log.1
-# so some information write at kern.log, it also need to dump
-#cat /var/log/kern.log |awk '{ for(i=1; i<=4; i++){ $i="" }; print $0 }'|sed 's/^    //g'
-cat /var/log/kern.log |cut -f5- -d ' '
+# dump the kernel information form target
+last_order=${1:-0}
+# check whether parameter 1 have the Non-Number
+[[ "${last_order//[0-9]/}" ]] && echo "$0 parameter 1 just support +N number, N start from 0" && builtin exit 1
+last_order=$[ $last_order + 1 ]
+log_file=${2:-"/var/log/kern.log"}
+# check whether target file is not exit
+[[ ! -f "$log_file" ]] && echo "$0 parameter 2 $log_file is not exist" && builtin exit 1
+declare -a line_lst
+# here line lst to keep 2 value, [0] is target line, [1] is end line
+# if target is last one, so will cut content form [0] -> file end
+# if target is second last, so will cut content from [0] -> [1]
+line_lst=($(sof-get-kernel-line.sh $log_file|tail -n $last_order|awk '{print $1;}'))
+# sof-get-kernel-line.sh dump nothing
+[[ ! "${line_lst[0]}" ]] && echo "$0 without catch system boot keyword" && builtin exit 1
+# check target is last one
+if [ ! "${line_lst[1]}" ];then
+    # tr '\0' ' ' to fix warning message: "command substitution: ignored null byte in input"
+    tail -n +${line_lst[0]} $log_file|tr '\0' ' '|cut -f5- -d ' '
+else
+    line_count=$[ ${line_lst[1]} - ${line_lst[0]} ]
+    # tr '\0' ' ' to fix warning message: "command substitution: ignored null byte in input"
+    tail -n +${line_lst[0]} $log_file|tr '\0' ' '|head -n $line_count|cut -f5- -d ' '
+fi
