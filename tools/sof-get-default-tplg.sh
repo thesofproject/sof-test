2dc1c0d4a4f5fb7978a0ae100966bfc5da1a6a41
Wu, BinX <binx.wu@intel.com>
2020-01-17 10:18:20 +0800

    tools: sof-get-default-tplg to fix load error TPLG
    change the log check just for current kernel
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>

--- tools/sof-get-default-tplg.sh
+++ tools/sof-get-default-tplg.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Current: load it from dmesg
+# Current: load TPLG file order:
+# dmesg
+# journalctl
+# /var/log/syslog (sof-kernel-dump.sh)
 # Future: possibly be loaded from elsewhere
 # Notice: Only verified on Ubuntu 18.04
-tplg_file=$(journalctl -k |grep topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+tplg_file=$(dmesg |grep topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ ! "$tplg_file" ]] && \
-    tplg_file=$(grep topology /var/log/kern.log|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+    tplg_file=$(journalctl -k |grep topology |awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ ! "$tplg_file" ]] && \
-    tplg_file=$(grep topology /var/log/syslog|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+    tplg_file=$(sof-kernel-dump.sh |grep topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ "$tplg_file" ]] && basename $tplg_file || echo ""
