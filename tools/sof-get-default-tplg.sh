116b75eaf75ca868e8d238ae36a06a284e42ceec
DQ <daniel@chaosengine.net>
2020-03-19 08:54:33 -0700

    use case-insensitive grep for "topology"
    kernel v5.5 uses "Topology", previous versions use "topology"

--- tools/sof-get-default-tplg.sh
+++ tools/sof-get-default-tplg.sh
@@ -6,9 +6,9 @@
 # /var/log/syslog (sof-kernel-dump.sh)
 # Future: possibly be loaded from elsewhere
 # Notice: Only verified on Ubuntu 18.04
-tplg_file=$(dmesg |grep topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+tplg_file=$(dmesg |grep -i topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ ! "$tplg_file" ]] && \
-    tplg_file=$(journalctl -k |grep topology |awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+    tplg_file=$(journalctl -k |grep -i topology |awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ ! "$tplg_file" ]] && \
-    tplg_file=$(sof-kernel-dump.sh |grep topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
+    tplg_file=$(sof-kernel-dump.sh |grep -i topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ "$tplg_file" ]] && basename $tplg_file || echo ""
