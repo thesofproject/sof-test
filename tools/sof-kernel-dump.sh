a447826d17f792a4dfe753e3c2aa2ea1577dde51
Wu, BinX <binx.wu@intel.com>
2020-03-25 14:23:23 +0800

    tools: fix sof-get-kernel-line.sh not in PATH error
    As sof-get-kernel-line.sh is not in PATH, the direct
    calling of it from sof-kernel-dump.sh will fail with
    command not found error.
    
    This patch adds code to find sof-get-kernel-line.sh
    before calling it.
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>

--- tools/sof-kernel-dump.sh
+++ tools/sof-kernel-dump.sh
@@ -8,11 +8,20 @@ last_order=$[ $last_order + 1 ]
 log_file=${2:-"/var/log/kern.log"}
 # check whether target file exists
 [[ ! -f "$log_file" ]] && echo "$0 parameter 2 $log_file does not exist" && builtin exit 1
+# load helper script
+which sof-get-kernel-line.sh 2>&1 1>/dev/null
+if [ $? -eq 0 ]; then
+    kernel_line_script="sof-get-kernel-line.sh"
+else
+    cd $(dirname $0)
+    kernel_line_script=$PWD/sof-get-kernel-line.sh
+    cd $OLDPATH
+fi
 declare -a line_lst
 # here line lst to keep 2 value, [0] is target line, [1] is end line
 # if target is last one, so will cut content form [0] -> file end
 # if target is second last, so will cut content from [0] -> [1]
-line_lst=($(sof-get-kernel-line.sh $log_file|tail -n $last_order|awk '{print $1;}'))
+line_lst=($($kernel_line_script $log_file|tail -n $last_order|awk '{print $1;}'))
 # sof-get-kernel-line.sh dump nothing
 [[ ! "${line_lst[0]}" ]] && echo "$0 without catch system boot keyword" && builtin exit 1
 # check target is last one
