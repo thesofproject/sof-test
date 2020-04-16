thesofproject/sof-test

commit faadb20a854834dddabd794b436cb7e693a9a836

CI: travis: new shellcheck-gitrange.bash
Travis passes $TRAVIS_COMMIT_RANGE to new shellcheck-gitrange.bash

Runs shellcheck only on files changed by the PR.

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

.travis.yml
@@ -1,0 +1,17 @@
+# bionic = 2018
+dist: bionic
+
+language: minimal
+
+git:
+  depth: false
+  quiet: true
+
+before_install:
+  - sudo apt-get -y install shellcheck
+
+jobs:
+  include:
+
+    - name: "shellcheck"
+      script: ./tools/shellcheck-gitrange.bash "${TRAVIS_COMMIT_RANGE}"

tools/shellcheck-gitrange.bash
@@ -1,0 +1,36 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2018 Intel Corporation. All rights reserved.
+
+set -e
+
+# Sample usage:
+#
+#   $  shellcheck-gitrange.bash HEAD~5.. [ -f gcc ]
+
+main()
+{
+    # The rest of args is passed as is to shellcheck
+    local gitrange="$1"; shift
+
+    printf '%s checking range %s\n\n' "$0" "$gitrange"
+    # also a sanity check
+    git log --oneline "$gitrange" -- | cat # no pager
+
+    local fname ftype failed_files=0
+
+    # https://mywiki.wooledge.org/BashFAQ/001
+    while IFS=  read -r fname; do
+
+        ftype=$(file --brief --mime-type "$fname")
+        if  [ x'text/x-shellscript' = x"$ftype" ]; then
+            printf '\n\n  ----- shellcheck %s ----\n\n' "$fname"
+            shellcheck "$@" "$fname"  || : $((failed_files++))
+        fi
+
+    done < <(git diff --name-only --diff-filter=d "$gitrange" -- )
+
+    return $failed_files
+}
+
+main "$@"

commit 71e84d913ecb6226787bd885602a8e3a37c11aee

tools/shellcheck: git log ${TRAVIS_COMMIT_RANGE/.../..}
$TRAVIS_COMMIT_RANGE contains triple dots,
e.g. "origin/master...HEAD". This is great for git diff but unsuitable
for git log. Convert any triple dots to double dots before passing to
git log (don't change the argument for git diff --name-only).

Also use a more verbose git log command and --no-pager instead of the
gross '|cat ': the sanity check was reporting issues but always
reporting success.

Fixes: faadb20a8548 ("CI: travis: new shellcheck-gitrange.bash")

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

.travis.yml
@@ -14,4 +14,5 @@ jobs:
   include:
 
     - name: "shellcheck"
+      # Triple-dotted 'origin/master...HEAD'
       script: ./tools/shellcheck-gitrange.bash "${TRAVIS_COMMIT_RANGE}"

tools/shellcheck-gitrange.bash
@@ -6,16 +6,22 @@ set -e
 
 # Sample usage:
 #
-#   $  shellcheck-gitrange.bash HEAD~5.. [ -f gcc ]
+#   $  shellcheck-gitrange.bash origin/master... [ -f gcc ]
 
 main()
 {
     # The rest of args is passed as is to shellcheck
-    local gitrange="$1"; shift
+    local diffrange="$1"; shift
 
-    printf '%s checking range %s\n\n' "$0" "$gitrange"
-    # also a sanity check
-    git log --oneline "$gitrange" -- | cat # no pager
+    printf '%s checking diff range: %s\n\n' "$0" "$diffrange"
+
+    # Triple dot "git log A...B" includes commits not relevant to triple
+    # dot "git diff A...B"
+    local logrange=${diffrange/.../..}
+    ( set -x
+      # also a sanity check of the argument
+      git --no-pager log --oneline --graph --stat --max-count=40 "$logrange" --
+    )
 
     local fname ftype failed_files=0
 
@@ -28,7 +34,7 @@ main()
             shellcheck "$@" "$fname"  || : $((failed_files++))
         fi
 
-    done < <(git diff --name-only --diff-filter=d "$gitrange" -- )
+    done < <(git diff --name-only --diff-filter=d "$diffrange" -- )
 
     return $failed_files
 }