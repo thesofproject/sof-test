78cb5ee21e5978f86c51cf99004ba661c57bc897
Wu, BinX <binx.wu@intel.com>
2020-05-09 13:02:31 +0800

    tools: sof-get-kernel-line remove 0x0 in grep result
    If Ubuntu19.10 system hanged, it may store '0x0' into
    /var/log/kern.log. Normally, '0x0' is at the beginning
    of a line.
    
    This causes our current log format command
    ```
    grep -na "$bootup_keyword" "$log_file"| \
        awk -F ':' '{print $1 $(NF-2);}'|  \
        sed "s/$bootup_keyword//g" | awk '{print $1, $2;}'
    ```
     not work as expected.
    
    This patch adds a sed filter to remove the redundant and
    useless '0x0'.
    
    Signed-off-by: Wu, BinX <binx.wu@intel.com>

--- tools/sof-get-kernel-line.sh
+++ tools/sof-get-kernel-line.sh
@@ -7,6 +7,6 @@
 # 2156 4.15.0-74-generic
 # 3246 4.15.0-74-generic
 log_file=${1:-"/var/log/kern.log"}
-[[ ! -f $log_file ]] && builtin exit 1
+[[ ! -f "$log_file" ]] && builtin exit 1
 bootup_keyword='\[    0.000000\] Linux version '
-grep -na "$bootup_keyword" $log_file|awk -F ':' '{print $1 $(NF-2);}'|sed "s/$bootup_keyword//g"|awk '{print $1, $2;}'
+grep -na "$bootup_keyword" "$log_file"|sed 's/\x0//g'|awk -F ':' '{print $1 $(NF-2);}'|sed "s/$bootup_keyword//g"|awk '{print $1, $2;}'
