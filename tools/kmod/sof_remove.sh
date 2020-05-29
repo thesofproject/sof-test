41ec87de0e9d160e8d84904904a7d3dbe1af6c1c
Bard Liao <yung-chuan.liao@linux.intel.com>
2020-05-29 14:17:41 +0800

    kmod: update rt5682_i2c codec driver name
    rt5682 i2c driver is spilt from rt5682 codec driver.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>

--- tools/kmod/sof_remove.sh
+++ tools/kmod/sof_remove.sh
@@ -80,6 +80,7 @@ remove_module snd_soc_rt5670
 remove_module snd_soc_rt5677
 remove_module snd_soc_rt5677_spi
 remove_module snd_soc_rt5682_sdw
+remove_module snd_soc_rt5682_i2c
 remove_module snd_soc_rt5682
 remove_module snd_soc_rl6231
 remove_module snd_soc_rl6347a
