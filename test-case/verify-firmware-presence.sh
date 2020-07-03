14b8d500d8f7d39c202e9e02937c9a79908dd6a3
Marc Herbert <marc.herbert@intel.com>
2020-07-03 13:23:29 -0700

    verify-firmware-presence.sh: missing quotes and other warnings
    No functional change.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- test-case/verify-firmware-presence.sh
+++ test-case/verify-firmware-presence.sh
@@ -14,7 +14,8 @@
 ##
 
 # source from the relative path of current folder
-source $(dirname ${BASH_SOURCE[0]})/../case-lib/lib.sh
+# shellcheck source=case-lib/lib.sh
+source "$(dirname "${BASH_SOURCE[0]}")"/../case-lib/lib.sh
 
 func_opt_parse_option "$@"
 
@@ -22,7 +23,5 @@ path=$(sof-dump-status.py -P)
 platform=$(sof-dump-status.py -p)
 fw="$path/sof-$platform.ri"
 dlogi "Checking file: $fw"
-[[ ! -f $fw ]] && die "File $fw is not found!"
-dlogi "Found file: $(md5sum $fw|awk '{print $2, $1;}')"
-
-exit 0
+[ -f "$fw" ] || die "File $fw is not found!"
+dlogi "Found file: $(md5sum "$fw"|awk '{print $2, $1;}')"
