42ccfa1d4d9eb7f7c78c549865975b9283269a51
Pan Xiuli <xiuli.pan@linux.intel.com>
2020-10-28 05:50:24 +0000

    test-case: check-ipc-flood: check kernel log in each iteration
    remove the redundant kernel log check for all case.
    enable kernel log check for each iteration.
    
    Signed-off-by: Pan Xiuli <xiuli.pan@linux.intel.com>

--- test-case/check-ipc-flood.sh
+++ test-case/check-ipc-flood.sh
@@ -32,27 +32,26 @@ loop_cnt=${OPT_VALUE_lst['l']}
 
 [[ ! "$(sof-kernel-dump.sh|grep 'sof-audio'|grep 'Firmware debug build')" ]] && dlogw "${BASH_SOURCE[0]} need debug version firmware" && exit 2
 
-func_lib_setup_kernel_checkpoint
 func_lib_check_sudo
 
 dlogi "Check sof debug fs environment"
 [[ "$(sudo file $ipc_flood_dfs|grep 'No such file')" ]] && dlogw "${BASH_SOURCE[0]} need $ipc_flood_dfs to run the test case" && exit 2
 dlogi "Checking ipc flood test!"
 
-# cleanup dmesg buffer before test
-sudo dmesg -c > /dev/null
-
 for i in $(seq 1 $loop_cnt)
 do
+    # TODO: use journalctl to replace dmesg
+    # cleanup dmesg buffer for each iteration
+    sudo dmesg -c > /dev/null
+    # set up timestamp for each iteration
+    func_lib_setup_kernel_checkpoint
     dlogi "===== [$i/$loop_cnt] loop Begin ====="
     dlogc "sudo bash -c 'echo $lpc_loop_cnt > $ipc_flood_dfs'"
     sudo bash -c "'echo $lpc_loop_cnt > $ipc_flood_dfs'"
 
-    sof-kernel-log-check.sh 0 || die "Caught error in dmesg"
-
+    # check kernel log for each iteration to catch issues
+    sof-kernel-log-check.sh "$KERNEL_CHECKPOINT" || die "Caught error in kernel log"
+    # TODO: use journalctl to replace dmesg
     dlogi "Dumping test logs!"
     dmesg | grep "IPC Flood count" -A 2
 done
-
-sof-kernel-log-check.sh "$KERNEL_CHECKPOINT"
-exit 0
