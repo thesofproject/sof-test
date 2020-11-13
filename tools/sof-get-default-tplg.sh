b10c004906160f3c636f56b6007177133a7427db
Marc Herbert <marc.herbert@intel.com>
2020-11-12 16:24:36 -0800

    sof-get-default-tplg: quote $tplg_file, last shellcheck warning
    Now shellcheck clean.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- tools/sof-get-default-tplg.sh
+++ tools/sof-get-default-tplg.sh
@@ -11,4 +11,4 @@ tplg_file=$(dmesg |grep -i topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
     tplg_file=$(journalctl -k |grep -i topology |awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
 [[ ! "$tplg_file" ]] && \
     tplg_file=$(sof-kernel-dump.sh |grep -i topology|awk -F ':' '/tplg/ {print $NF;}'|tail -n 1)
-[[ "$tplg_file" ]] && basename $tplg_file || echo ""
+[[ "$tplg_file" ]] && basename "$tplg_file" || echo ""
