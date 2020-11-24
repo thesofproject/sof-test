552de8f7fc337d78b1b29d967e1e4a7fee0f2d3c
Marc Herbert <marc.herbert@intel.com>
2020-11-23 18:01:54 -0800

    Re-add multiple-pipeline-capture.sh as a wrapper
    Not in previous commit otherwise git fails to track the rename which
    breaks git blame, cherry-pick, merge etc.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

---
+++ test-case/multiple-pipeline-capture.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+TESTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+
+# Need "exec" otherwise it believes to be a Sub-Test
+exec "$TESTDIR"/test-case/multiple-pipeline.sh "$@"
