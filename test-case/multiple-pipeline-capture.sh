235305392db047fddbc20123180cba7ecab37dca
Marc Herbert <marc.herbert@intel.com>
2020-11-23 18:29:24 -0800

    multiple-pipeline.sh: add new -f(irst) option to make it generic
    multiple-pipeline-capture.sh is now multiple-pipeline.sh -f c
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- test-case/multiple-pipeline-capture.sh
+++ test-case/multiple-pipeline-capture.sh
@@ -5,4 +5,4 @@ set -e
 TESTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 # Need "exec" otherwise it believes to be a Sub-Test
-exec "$TESTDIR"/test-case/multiple-pipeline.sh "$@"
+exec "$TESTDIR"/test-case/multiple-pipeline.sh -f c "$@"
