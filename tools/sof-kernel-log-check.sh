thesofproject/sof-test

commit b513877896af4cb29096ed1a2fdc6123665d1cec

sof-kernel-log-check: fix to filter out USB3.1 suspend/resume error
Refine ignore_str to match correctly.

Signed-off-by: Fred Oh <fred.oh@linux.intel.com>

tools/sof-kernel-log-check.sh
@@ -204,8 +204,8 @@ ignore_str="$ignore_str"'|usb 2-.: .'
 
 # TGL devices with USB 3.1 devices, issues reported by sof-test
 # BugLink: https://github.com/thesofproject/sof-test/issues/482
-ignore_str="$ignore_str"'|usb 3-.: device descriptor read/64, error .'
-ignore_str="$ignore_str"'|usb 3-.: device not accepting address ., error .'
+ignore_str="$ignore_str"'|usb 3-.+: device descriptor read/64, error .+'
+ignore_str="$ignore_str"'|usb 3-.+.: device not accepting address .+, error .+'
 
 # Test cases on some platforms fail because the boot retry message:
 #

commit 920e7ab10b52519f9ef0b722d9629bbe4c4c8c5e

sof-kernel-log-check: refine filter string for USB error
Remove the ending '.+' to avoid shadowing of preceeding
filter string.

Fixes: b5138778

Signed-off-by: Amery Song <chao.song@intel.com>

tools/sof-kernel-log-check.sh
@@ -204,8 +204,8 @@ ignore_str="$ignore_str"'|usb 2-.: .'
 
 # TGL devices with USB 3.1 devices, issues reported by sof-test
 # BugLink: https://github.com/thesofproject/sof-test/issues/482
-ignore_str="$ignore_str"'|usb 3-.+: device descriptor read/64, error .+'
-ignore_str="$ignore_str"'|usb 3-.+.: device not accepting address .+, error .+'
+ignore_str="$ignore_str"'|usb 3-.+: device descriptor read/64, error'
+ignore_str="$ignore_str"'|usb 3-.+.: device not accepting address .+, error'
 ignore_str="$ignore_str"'|usb usb.-port.+: unable to enumerate USB device'
 
 # Test cases on some platforms fail because the boot retry message: