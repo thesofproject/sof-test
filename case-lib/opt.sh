thesofproject/sof-test

commit 9f412894bf449bea34275d53725fc05a236e1ea1

opt.sh: remove leading space form case documentation
In our design, a line starts with '##' is regarded as
case documentation, we should also remove the leading
space together with '##'.

And also fix a typo in this commit.

Signed-off-by: Amery Song <chao.song@intel.com>

case-lib/opt.sh
@@ -29,9 +29,9 @@ func_opt_parse_option()
     local _op_temp_script
     local -A _op_short_lst _op_long_lst
 
-    _func_case_dump_descption()
+    _func_case_dump_description()
     {
-         grep '^##' "$SCRIPT_NAME"|sed 's/^##//g'
+         grep '^##' "$SCRIPT_NAME" | sed 's/^## //g'
     }
 
     # Asks getopt to validate command line input and generate $_op_temp_script
@@ -104,7 +104,7 @@ func_opt_parse_option()
 
             printf '    -h |  --help\n'
             printf '\tthis message\n'
-            _func_case_dump_descption
+            _func_case_dump_description
             trap - EXIT
             exit 2
         }
@@ -153,5 +153,5 @@ func_opt_parse_option()
         } >> "$LOG_ROOT/version.txt"
     fi
 
-    unset _func_create_tmpbash _func_opt_dump_help _func_case_dump_descption
+    unset _func_create_tmpbash _func_opt_dump_help _func_case_dump_description
 }

commit 1a8f829d01f837e0e81261aacadfc6219f30b669

opt.sh: fix case description
A previous fix 9f412894 helps to remove the
leading space of each line in the comments.

However, there is only "##" in the first and last
line of the comments, which doesn't match "^## ",
and thus is not removed.

This patch first removes the leading "##", and then
the leading space.

Fixes: 9f412894

Signed-off-by: Chao Song <chao.song@intel.com>

case-lib/opt.sh
@@ -30,11 +30,11 @@ add_common_options()
 # and dump help
 func_opt_parse_option()
 {
-    # lines that start with '##' will be regarded as documentation.
-    # this function dumps these lines after removing the leading '## '
+    # Lines that start with '##' will be regarded as documentation,
+    # removing the leading '##' or '## ' from documentation.
     _dump_case_description()
     {
-        grep '^##' "$SCRIPT_NAME" | sed 's/^## //g'
+        grep '^##' "$SCRIPT_NAME" | sed 's/^##//g' | sed 's/^ //g'
     }
 
     # This function helps to fill below four variables