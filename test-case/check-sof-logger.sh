44bba1bdb5c88c7b17d9bf2951041d8d549ba438
Marc Herbert <marc.herbert@intel.com>
2021-03-12 18:28:09 -0800

    check-sof-logger: fix quoting, other shellcheck and rephrase errors
    Hopefully no functional change.
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- test-case/check-sof-logger.sh
+++ test-case/check-sof-logger.sh
@@ -25,16 +25,16 @@ func_opt_parse_option "$@"
 setup_kernel_check_point
 
 # check sof-logger location
-if [ -z $(which sof-logger) ]; then
+type -a sof-logger ||
     die "sof-logger Not Installed!"
-fi
 
-if [ $(which -a sof-logger|wc -l) -ne 1 ]; then
+
+if type -a sof-logger | tail -n +2 | grep -q . ; then
     dlogw "There are multiple sof-loggers in system"
-    dlogw "using " `which sof-logger`
+    dlogw "using $(type -p sof-logger)"
 fi
-loggerBin=$(which sof-logger)
-dlogi "Found file: $(md5sum $loggerBin|awk '{print $2, $1;}')"
+loggerBin=$(type -p sof-logger)
+dlogi "Found file: $(md5sum "$loggerBin" | awk '{print $2, $1;}')"
 
 dlogi "Looking for ldc File ..."
 ldcFile=$(find_ldc_file) || die ".ldc file not found!"
@@ -51,20 +51,20 @@ dlogi "Try to dump the dma trace log via sof-logger ..."
 dlogc "sudo $loggerBin -t -l $ldcFile -o $data_file 2> $error_file &"
 sudo bash -c "'$loggerBin -t -l $ldcFile -o $data_file 2> $error_file &'"
 sleep 2
-dlogc "sudo pkill -9 $(basename $loggerBin)"
-sudo pkill -9 $(basename $loggerBin) 2> /dev/null
+dlogc "sudo pkill -9 $(basename "$loggerBin")"
+sudo pkill -9 "$(basename "$loggerBin")" 2> /dev/null
 
 func_logger_exit()
 {
     local code=$1 type=${2:-data}
     dlogi "Log $type BEG>>"
-    cat $LOG_ROOT/logger.$type.log
+    cat "$LOG_ROOT/logger.$type.log"
     dlogi "<<END $type data"
-    exit $code
+    exit "$code"
 }
 
 # check if we get any sof-logger errors
-logger_err=`grep -i "error" $error_file`
+logger_err=$(grep -i 'error' "$error_file")
 if [[ $logger_err ]]; then
     dloge "No available log to export due to sof-logger errors."
     func_logger_exit 1 'error'
@@ -72,19 +72,20 @@ fi
 
 # '\.c\:[1-9]' to filter like '.c:6' this type keyword like:
 # [3017136.770833]  (11.302083) c0 SA  src/lib/agent.c:65  ERROR validate(), ll drift detected, delta = 25549
-fw_log_err=`grep -i "error" $data_file | grep -v '\.c\:[1-9]'`
+fw_log_err=$(grep -i 'error' "$data_file" | grep -v '\.c\:[1-9]')
+
 # '[[:blank:]]TIMESTAMP.*CONTENT$' to filter the log header:
 # TIMESTAMP  DELTA C# COMPONENT  LOCATION  CONTENT
-if [[ ! $(sed -n '/[[:blank:]]TIMESTAMP.*CONTENT$/p' $data_file) ]]; then
-    dloge "No available log to export."
+if [[ ! $(sed -n '/[[:blank:]]TIMESTAMP.*CONTENT$/p' "${data_file}") ]]; then
+    dloge "Log header not found in ${data_file}"
     func_logger_exit 1
 # we catch error from fw log
 elif [[ $fw_log_err ]]; then
-    dloge "Errors in firmware log:"
+    dloge "Error(s) found in firmware log ${data_file}"
     func_logger_exit 1
 fi
 
-if [ "$(grep -i 'error' $data_file)" ]; then
+if grep -i -q 'error' "$data_file" ; then
     dlogw "Catch keyword 'ERROR' in firmware log"
 fi
 
