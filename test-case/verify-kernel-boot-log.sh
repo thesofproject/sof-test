bf852427761e465dec355c0433332eb16964cdab
Chao Song <chao.song@linux.intel.com>
2021-04-20 10:53:11 +0800

    sof-test: add a test case to check kernel boot log
    Signed-off-by: Chao Song <chao.song@linux.intel.com>

---
+++ test-case/verify-kernel-boot-log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##
+## Case Name: verify-kernel-boot-log
+## Preconditions:
+##    N/A
+## Description:
+##    Check kernel boot log, and see if there is any errors
+## Case step:
+##    1. Disable kernel check point and check kernel log from kernel boot
+## Expect result:
+##    No error found in kernel boot log
+##
+
+set -e
+
+# source from the relative path of current folder
+# shellcheck source=case-lib/lib.sh
+source "$(dirname "${BASH_SOURCE[0]}")"/../case-lib/lib.sh
+
+func_opt_parse_option "$@"
+disable_kernel_check_point
+
+sof-kernel-log-check.sh
