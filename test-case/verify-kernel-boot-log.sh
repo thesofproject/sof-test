1c25a77323ceed0c29fdf6462356d2d73379dcc4
Marc Herbert <marc.herbert@intel.com>
2021-10-14 21:20:10 -0700

    verify-kernel-boot-log: move all the code to functions
    Rationale in #740
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- test-case/verify-kernel-boot-log.sh
+++ test-case/verify-kernel-boot-log.sh
@@ -18,26 +18,36 @@ set -e
 # shellcheck source=case-lib/lib.sh
 source "$(dirname "${BASH_SOURCE[0]}")"/../case-lib/lib.sh
 
-func_opt_parse_option "$@"
-disable_kernel_check_point
+main()
+{
+    func_opt_parse_option "$@"
+    disable_kernel_check_point
 
-print_module_params
+    print_module_params
 
-# Check this device time is NTP Synchronized
-if check_ntp_sync; then
-    printf '\nTime Check: NTP Synchronized\n'
-else
-    timedatectl show
-    # try to disable/enable NTP once, this will trigger ntp sync twice,
-    # before stopping ntp and after enabling ntp.
-    re_enable_ntp_sync
+    ntp_check
 
+    sof-kernel-log-check.sh
+}
+
+ntp_check()
+{
+    # Check this device time is NTP Synchronized
     if check_ntp_sync; then
-        printf '\nTime Check: NTP Synchronized after re-enabling ntp sync\n'
+        printf '\nTime Check: NTP Synchronized\n'
     else
-        # If NTP is not synchronized, let this test fail
-        die "Time Check: NTP NOT Synchronized"
-    fi
-fi
+        timedatectl show
+        # try to disable/enable NTP once, this will trigger ntp sync twice,
+        # before stopping ntp and after enabling ntp.
+        re_enable_ntp_sync
 
-sof-kernel-log-check.sh
+        if check_ntp_sync; then
+            printf '\nTime Check: NTP Synchronized after re-enabling ntp sync\n'
+        else
+            # If NTP is not synchronized, let this test fail
+            die "Time Check: NTP NOT Synchronized"
+        fi
+    fi
+}
+
+main "$@"
