thesofproject/sof-test

commit 814b904acfca65dbebb869b0ab96735dc9a51cb0

lib.sh: fix sudo "$loggerCmd" quoting
sudo "$loggerCmd" works only with our redefined sudo function. Replace
it with an array-based and properly quoted sudo "${loggerCmd[@]}" that
works with any sudo.

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

case-lib/lib.sh
@@ -140,10 +140,10 @@ func_lib_start_log_collect()
         return 3
     fi
 
-    local loggerCmd="$SOFLOGGER $logopt -l $ldcFile -o $logfile"
-    dlogi "Starting $loggerCmd"
+    local loggerCmd=("$SOFLOGGER" "$logopt" -l "$ldcFile" -o "$logfile")
+    dlogi "Starting ${loggerCmd[*]}"
     # Cleaned up by func_exit_handler() in hijack.sh
-    sudo "$loggerCmd" &
+    sudo "${loggerCmd[@]}" &
 }
 
 # Calling this function is often a mistake because the error message

commit 1f10fa84c627bcedde22da0b781b1ffd78a0225c

lib.sh: fix etrace collection on failure (recent regression)
Fixes failures to collect etrace on failure. It goes like this:

UTC [INFO] Starting sof-logger '' -l /lib/firmware/intel/sof/sof-apl.ldc
 -o sof-test/logs/check-runtime-pm-status/2021-12-02-23:49:42-22493/etrace.txt
error: Unused parameter ''
Usage sof-logger <option(s)> <file(s)>

This happens when $logopt is empty.

Fixes recent regression introduced by commit 814b904acfca6 ("lib.sh: fix
sudo "$loggerCmd" quoting")

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

case-lib/lib.sh
@@ -142,7 +142,9 @@ func_lib_start_log_collect()
         return 3
     fi
 
-    local loggerCmd=("$SOFLOGGER" "$logopt" -l "$ldcFile" -o "$logfile")
+    # The logger does not like empty '' arguments and $logopt can be
+    # shellcheck disable=SC2206
+    local loggerCmd=("$SOFLOGGER" $logopt -l "$ldcFile" -o "$logfile")
     dlogi "Starting ${loggerCmd[*]}"
     # Cleaned up by func_exit_handler() in hijack.sh
     sudo "${loggerCmd[@]}" &