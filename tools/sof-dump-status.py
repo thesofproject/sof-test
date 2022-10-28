97141585a888ae0959d1c7b463e2027c0db6588a
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2022-10-28 22:36:46 +0300

    sof-dump-status.py: add ADL-N PCI DID
    Add PCI DID for ADL-N.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- tools/sof-dump-status.py
+++ tools/sof-dump-status.py
@@ -37,7 +37,8 @@ class clsSYSCardInfo():
             # pci-icl.c
             "0x34c8":"icl", "0x38c8":"jsl", "0x4dc8":"jsl",
             # pci-tgl.c
-            "0xa0c8":"tgl", "0x43c8":"tgl-h", "0x4b55":"ehl", "0x4b58":"ehl", "0x7ad0": "adl-s", "0x51c8":"adl",
+            "0xa0c8":"tgl", "0x43c8":"tgl-h", "0x4b55":"ehl", "0x4b58":"ehl", "0x7ad0": "adl-s",
+            "0x51c8":"adl", "0x54c8":"adl-n",
             # pci-mtl.c
             "0x7e28":"mtl",
             # Other PCI IDs
