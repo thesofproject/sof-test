02aafc01dafaa498e4e27bc32adcb8d3bf91d97e
Keqiao Zhang <keqiao.zhang@intel.com>
2023-03-29 14:35:34 +0800

    Amixer setting for ipc4 platforms (#1012)
    * amixer_settings: adjust amixer settings for IPC4 platforms
    
    Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>
    
    * lib.sh: switch to use reset_sof_volume funtion to adjust
    amixer settings for IPC4 nocodec platforms
    
    Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>
    
    ---------
    
    Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>

--- alsa_settings/MTLP_RVP_HDA.sh
+++ alsa_settings/MTLP_RVP_HDA.sh
@@ -5,6 +5,7 @@ amixer -c sofhdadsp cset name='Master Playback Switch' 1
 amixer -c sofhdadsp cset name='Master Playback Volume' 87
 amixer -c sofhdadsp cset name='Headphone Playback Switch' 1
 amixer -c sofhdadsp cset name='Headphone Playback Volume' 60
-amixer -c sofhdadsp cset name='gain.1.1 1 2nd Playback Volume' 45
-amixer -c sofhdadsp cset name='gain.2.1 2 Main Playback Volume' 45
-amixer -c sofhdadsp cset name='gain.5.1 5 3nd Playback Volume' 45
+
+# enable headset capture
+amixer -c sofhdadsp cset name='Capture Switch' on
+amixer -c sofhdadsp cset name='Capture Volume' 30
