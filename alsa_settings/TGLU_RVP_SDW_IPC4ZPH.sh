cd873b8200bc75f4c65b6b36b29bc5774110f450
Keqiao Zhang <keqiao.zhang@intel.com>
2023-04-11 10:24:48 +0800

    alsa_settings: adjust amixer setting for TGLU_RVP_SDW_IPC4ZPH
    Set the 'rt711 AMIC Volume' to 0 and turn up the volume of
    'rt711 ADC 08 Capture Volume' a bit.
    
    Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>

--- alsa_settings/TGLU_RVP_SDW_IPC4ZPH.sh
+++ alsa_settings/TGLU_RVP_SDW_IPC4ZPH.sh
@@ -7,4 +7,5 @@ amixer -c sofsoundwire cset name='rt711 DAC Surr Playback Volume' 55
 # enable headset capture
 amixer -c sofsoundwire cset name='Headset Mic Switch' on
 amixer -c sofsoundwire cset name='rt711 ADC 08 Capture Switch' on
-amixer -c sofsoundwire cset name='rt711 ADC 08 Capture Volume' 25
+amixer -c sofsoundwire cset name='rt711 ADC 08 Capture Volume' 45
+amixer -c sofsoundwire cset name='rt711 AMIC Volume' 0
