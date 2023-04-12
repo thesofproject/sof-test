efb27ccd3011233d7a5a16964f5f984c1d05d51b
Fred Oh <fred.oh@linux.intel.com>
2023-04-12 15:25:58 -0700

    alsa_settings: change amixer settings for MTLP_RVP_SDW
    Overall amplitude of the captured waveform is about 50%.
    
    Signed-off-by: Fred Oh <fred.oh@linux.intel.com>

--- alsa_settings/MTLP_RVP_SDW.sh
+++ alsa_settings/MTLP_RVP_SDW.sh
@@ -6,5 +6,5 @@ amixer -c sofsoundwire cset name='rt711 FU05 Playback Volume' 60
 
 # enable headset capture
 amixer -c sofsoundwire cset name='Headset Mic Switch' on
-amixer -c sofsoundwire cset name='rt711 ADC 08 Capture Switch' on
-amixer -c sofsoundwire cset name='rt711 ADC 08 Capture Volume' 25
+amixer -c sofsoundwire cset name='rt711 FU0F Capture Switch' on
+amixer -c sofsoundwire cset name='rt711 FU0F Capture Volume' 43
