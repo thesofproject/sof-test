b971d9854fbbee065454676a4685e37091993b65
Keqiao Zhang <keqiao.zhang@intel.com>
2023-06-08 09:43:35 +0800

    alsa_settings: adjust the volume for MTL_RVP_SDW
    Since we have replaced the USB sound card to UCA222, so need to
    adjust the alsa settings to adapte to the new one.
    
    Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>

--- alsa_settings/MTLP_RVP_SDW.sh
+++ alsa_settings/MTLP_RVP_SDW.sh
@@ -2,9 +2,9 @@ set -e
 
 # enable headset playback
 amixer -c sofsoundwire cset name='Headphone Switch' on
-amixer -c sofsoundwire cset name='rt711 FU05 Playback Volume' 60
+amixer -c sofsoundwire cset name='rt711 FU05 Playback Volume' 80
 
 # enable headset capture
 amixer -c sofsoundwire cset name='Headset Mic Switch' on
 amixer -c sofsoundwire cset name='rt711 FU0F Capture Switch' on
-amixer -c sofsoundwire cset name='rt711 FU0F Capture Volume' 43
+amixer -c sofsoundwire cset name='rt711 FU0F Capture Volume' 30
