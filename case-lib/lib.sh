thesofproject/sof-test

commit 23c02a963c7ff3ed14d84b17ec2c6b9e8a9c37aa

alsa_settings: update the test config names
Signed-off-by: Keqiao Zhang <keqiao.zhang@intel.com>

case-lib/lib.sh
@@ -916,7 +916,7 @@ set_alsa_settings()
     local PNAME="${1%_ZEPHYR}"
     dlogi "Run alsa setting for $PNAME"
     case $PNAME in
-        APL_UP2_NOCODEC | CML_RVP_NOCODEC | JSL_RVP_NOCODEC | TGLU_RVP_NOCODEC | ADLP_RVP_NOCODEC | TGLH_RVP_NOCODEC | ADLP_RVP_NOCODEC-sof-ipc3-nocodec | CML_RVP_NOCODEC-sof-ipc3-nocodec | JSL_RVP_NOCODEC-sof-ipc3-nocodec)
+        APL_UP2_NOCODEC | CML_RVP_NOCODEC | JSL_RVP_NOCODEC | TGLU_RVP_NOCODEC | ADLP_RVP_NOCODEC | TGLH_RVP_NOCODEC | ADLP_RVP_NOCODEC-ipc3 | CML_RVP_NOCODEC-ipc3 | JSL_RVP_NOCODEC-ipc3)
             # common nocodec alsa settings
             "$SCRIPT_HOME"/alsa_settings/CAVS_NOCODEC.sh
         ;;
@@ -924,7 +924,7 @@ set_alsa_settings()
             # common nocodec_ci alsa settings
             "$SCRIPT_HOME"/alsa_settings/CAVS_NOCODEC_CI.sh
         ;;
-        TGLU_RVP_NOCODEC_IPC4ZPH | ADLP_RVP_NOCODEC_IPC4ZPH | MTLP_RVP_NOCODEC |ADLP_RVP_NOCODEC-cavs2_5-ipc4-nocodec | TGLU_RVP_NOCODEC-cavs2_5-ipc4-nocodec | MTLP_RVP_NOCODEC-ace1_0-ipc4-nocodec | MTLP_RVP_NOCODEC-ace1_0-multicore-2cores | MTLP_RVP_NOCODEC-ace1_0-multicore-3cores)
+        TGLU_RVP_NOCODEC_IPC4ZPH | ADLP_RVP_NOCODEC_IPC4ZPH | MTLP_RVP_NOCODEC |ADLP_RVP_NOCODEC-ipc4 | TGLU_RVP_NOCODEC-ipc4 | MTLP_RVP_NOCODEC | MTLP_RVP_NOCODEC-multicore-2cores | MTLP_RVP_NOCODEC-multicore-3cores)
             dlogi "Use reset_sof_volume function to set amixer setting."
 	;;
         *)

alsa_settings/MTLP_RVP_HDA-ace1_0-ipc4.sh
@@ -1,1 +1,0 @@
-MTLP_RVP_HDA.sh

commit bc92793746c8329b6c0a6beaa5c9c46fb7b107ae

lib.sh: set_alsa_settings(): remove duplicate  "MTLP_RVP_NOCODEC"
Fixes (giant) commit 23c02a963c7f ("alsa_settings: update the test
config names") which accidentally duplicated  "MTLP_RVP_NOCODEC"

Fixes shellcheck warning SC2221.

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

case-lib/lib.sh
@@ -929,7 +929,7 @@ set_alsa_settings()
             # common nocodec_ci alsa settings
             "$SCRIPT_HOME"/alsa_settings/CAVS_NOCODEC_CI.sh
         ;;
-        TGLU_RVP_NOCODEC_IPC4ZPH | ADLP_RVP_NOCODEC_IPC4ZPH | MTLP_RVP_NOCODEC |ADLP_RVP_NOCODEC-ipc4 | TGLU_RVP_NOCODEC-ipc4 | MTLP_RVP_NOCODEC | MTLP_RVP_NOCODEC-multicore-2cores | MTLP_RVP_NOCODEC-multicore-3cores | LNLM_RVP_NOCODEC)
+        TGLU_RVP_NOCODEC_IPC4ZPH | ADLP_RVP_NOCODEC_IPC4ZPH | ADLP_RVP_NOCODEC-ipc4 | TGLU_RVP_NOCODEC-ipc4 | MTLP_RVP_NOCODEC | MTLP_RVP_NOCODEC-multicore-2cores | MTLP_RVP_NOCODEC-multicore-3cores | LNLM_RVP_NOCODEC)
             dlogi "Use reset_sof_volume function to set amixer setting."
 	;;
         *)