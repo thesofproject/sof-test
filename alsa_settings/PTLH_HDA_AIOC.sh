f0293d461a0acb25ca47d5ee5177fe0387c56945
Mohsin Khan <mohsin.khan@intel.com>
2025-01-13 12:22:29 +0530

    alsa_settings/PTLH_HDA_AIOC.sh: Add alsabat mixer settings for PTL HDA
    Signed-off-by: Mohsin Khan <mohsin.khan@intel.com>

---
+++ alsa_settings/PTLH_HDA_AIOC.sh
@@ -0,0 +1,13 @@
+set -e
+
+# enable headset playback
+amixer -c sofhdadsp cset name='Master Playback Switch' 1
+amixer -c sofhdadsp cset name='Master Playback Volume' 87
+amixer -c sofhdadsp cset name='Headphone Playback Switch' 1
+amixer -c sofhdadsp cset name='Headphone Playback Volume' 60
+
+# enable headset capture
+amixer -c sofhdadsp cset name='Capture Switch' on
+amixer -c sofhdadsp cset name='Capture Volume' 30
+~
+
