863ab706616d125d0ce8b41d7c3f23265d3d2615
Wilczak <arturx.wilczak@intel.com>
2025-05-30 11:21:51 +0200

    FIX: alsa_settings for PTLH_SDW_RT712
    Change the default playback volume settings for PTLH SDW RT712.
    Reduce volume from 80 to 60.
    The level sets to 80 for these controls is a reason of failing
    the check-alsabat-headset-* tests.
    
    Internal Jira: SOFC2-1031
    
    Signed-off-by: Artur Wilczak <arturx.wilczak@intel.com>

--- alsa_settings/PTLH_SDW_RT712.sh
+++ alsa_settings/PTLH_SDW_RT712.sh
@@ -1,10 +1,8 @@
 set -e
 
 amixer -c sofsoundwire cset name='Headphone Switch' on
-amixer -c sofsoundwire cset name='rt712 FU05 Playback Volume' 80
-amixer -c sofsoundwire cset name='rt712 FU06 Playback Volume' 80
-
-
+amixer -c sofsoundwire cset name='rt712 FU05 Playback Volume' 60
+amixer -c sofsoundwire cset name='rt712 FU06 Playback Volume' 60
 
 # enable headset playback and capture
 amixer -c sofsoundwire cset name='Headset Mic Switch' on
