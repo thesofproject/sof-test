a86707632974bdff0915fc42b603e8d0fb1d3d81
Emilia Kurdybelska <emiliax.kurdybelska@intel.com>
2025-08-13 11:19:19 +0200

    test: add option to test with pipewire
    Signed-off-by: Emilia Kurdybelska <emiliax.kurdybelska@intel.com>

--- case-lib/hijack.sh
+++ case-lib/hijack.sh
@@ -12,6 +12,10 @@ function func_exit_handler()
 
     dlogi "Starting func_exit_handler($exit_status)"
 
+    if [ "$TEST_WITH_PIPEWIRE" == true ]; then
+        func_lib_disable_pipewire
+    fi
+
     # call trace
     if [ "$exit_status" -ne 0 ] ; then
         dloge "Starting ${FUNCNAME[0]}(), exit status=$exit_status, FUNCNAME stack:"
